e824fbbf815b536be6dcf857e8d4e17d6a80a088
Vladimir Dimitrov <v.dimitrov@g.nsu.ru>
2023-12-23 16:14:59 +0300

    add notebooks, scripts

---
+++ scripts/seed.py
@@ -0,0 +1,13 @@
+import random 
+import torch
+import os
+import numpy as np
+
+
+def seed_everything(seed=100):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
